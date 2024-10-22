76a82592993ad9fe3572d87735773d8d1b028587
RCSowjanya <145266658+RCSowjanya@users.noreply.github.com>
2024-10-22 17:01:38 +0530

    Update InNavbar.jsx

--- src/Components/InNavbar.jsx
+++ src/Components/InNavbar.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-
+import { FaPlane } from "react-icons/fa";
+import ApplyNowButton from "./ApplyNowButton";
 const Navbar = () => {
   return (
     <div>
       {/* Navbar Section */}
       <nav
-        className="bg-black text-white fixed top-0 left-0 right-0 z-50 py-3"
-        style={{ height: "60px" }}
+        className="bg-black text-white fixed top-0 left-0 right-0 z-50 py-2"
+        style={{ height: "4rem" }}
       >
-        <div className="px-[2rem] flex items-center justify-between">
+        <div className="px-[2rem] flex items-center justify-between h-full">
           {/* Left: Logo */}
-          <div className="text-xl font-bold">LOGO</div>
+          <div className="text-xl font-bold flex items-center h-full">LOGO</div>
 
           {/* Right: Apply Now Button */}
-          <div>
-            <button className="bg-[#00cc99] text-white px-8 py-1 rounded-full hover:bg-gray-300 transition">
+          <div className="flex items-center h-full">
+            <button className="bg-[#00cc99] text-white px-6 py-2 rounded-full hover:bg-gray-300 transition flex items-center gap-2">
+              <div className="p-2 rounded-full bg-gray-200 animate-button hidden md:block">
+                <FaPlane className="text-black" />
+              </div>
+              {/* Airplane icon */}
               Apply Now
             </button>
           </div>
